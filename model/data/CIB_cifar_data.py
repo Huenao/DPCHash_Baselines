24e5e3baca27ef24136c3ce4e754f66311c8ef89
hwt <1602787256@qq.com>
2023-03-28 14:00:43 +0800

    .

--- model/data/CIB_cifar_data.py
+++ model/data/CIB_cifar_data.py
@@ -26,6 +26,7 @@ class Data:
                                             transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]) 
                                             ])
         self.test_transforms = transforms.Compose([
+                                            transforms.Resize((224, 224)),
                                             transforms.ToTensor(),
                                             # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])     
                                             transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])                                
