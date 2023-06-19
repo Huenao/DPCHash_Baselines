564ddbb5df7a649009388dd839478061f4f1b6b7
Huenao <1602787256@qq.com>
2023-06-19 19:11:48 +0800

    add nuswide21 pr curves

--- utils/utils.py
+++ utils/utils.py
@@ -7,14 +7,22 @@ import torch
 from tqdm import tqdm
 
 
+# 50000
 cifar_d_range = [i for i in range(0, 10000, 500)] + [i for i in range(10000, 50001, 2500)]
 cifar_d_range[0] = 1
+# 117218
 coco_d_range = [i for i in range(0, 20000, 1000)] + [i for i in range(20000, 100001, 5000)] + [105000, 110000, 115000, 117218]
 coco_d_range[0] = 1
+# 181577
 nuswide_10_d_range = [i for i in range(0, 30000, 1500)] + [i for i in range(40000, 175001, 7500)] + [180000, 181577]
 nuswide_10_d_range[0] = 1
+# 193734
+nuswide_21_d_range = [i for i in range(0, 30000, 1500)] + [i for i in range(40000, 175001, 7500)] + [180000, 187500, 193734]
+nuswide_21_d_range[0] = 1
+# 23000
 flickr_d_range = [i for i in range(0, 5000, 250)] + [i for i in range(5000, 22501, 1250)] + [23000]
 flickr_d_range[0] = 1
+
 
 class MyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -164,6 +172,14 @@ def evalModel(test_loader, dataset_loader, net, Best_mAP, bit, config, epoch, f)
 
             elif "nuswide_10" in config["dataset"]:
                 P, R = pr_curve(trn_binary.numpy(), tst_binary.numpy(), trn_label.numpy(), tst_label.numpy(), draw_range=nuswide_10_d_range)
+                
+                print(f'Precision Recall Curve data:\n"{config["info"]}":[{P},{R}],')
+                f.write('PR | Epoch %d | ' % (epoch))
+                f.write(f'[{P}, {R}]')
+                f.write('\n')
+
+            elif "nuswide_21" in config["dataset"]:
+                P, R = pr_curve(trn_binary.numpy(), tst_binary.numpy(), trn_label.numpy(), tst_label.numpy(), draw_range=nuswide_21_d_range)
                 
                 print(f'Precision Recall Curve data:\n"{config["info"]}":[{P},{R}],')
                 f.write('PR | Epoch %d | ' % (epoch))
